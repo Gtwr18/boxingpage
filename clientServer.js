fa4262433033e7bf42d66f1ed0294c57bb7645af
Gtwr18 <dltkddn0323@snu.ac.kr>
2019-07-29 21:22:39 +0900

    [completed] boxingpage [need to be added] labelingpage

--- clientServer.js
+++ clientServer.js
@@ -1,23 +1,22 @@
 const express = require('express');
 const app = express();
-const http = require("http");
-const homepage = require("./homepage.js");
-app.use(express.static('public'));
+// const ipfilter = require('express-ipfilter').IpFilter
+// const ips = ['10.9.0.104'];
+// app.use(ipfilter(ips,{mode:'allow'}));
+app.use(express.static(__dirname + '/public'));
 
 // start the express web server listening on 8080
-app.listen(8080, () => {
+app.listen(8080, "0.0.0.0",() => {
   console.log('listening on 8080');
 });
 
 // serve the homepage
 app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/loginpage.html');
+    res.sendFile(__dirname + '/public/loginpage.html');
 });
 app.get('/homepage',(req,res)=>{
-    console.log("it came");
-    res.sendFile(__dirname + "/homepage.html");
+    res.sendFile(__dirname + "/public/homepage.html");
 });
 app.get('/boxingpage',function(req,res){
-  console.log('to boxingpage');
-  res.sendFile(__dirname + "/boxingpage.html");
-})
+  res.sendFile(__dirname + "/public/boxingpage.html");
+});
